973f85ab0d5e379138e390d6a6c45e69d8906602
Lee Hyung Won <ijhwlee@gmail.com>
2024-05-18 00:22:49 +0900

    add numeric examples

--- y2024/sources/week11/numeric/numeric.cpp
+++ y2024/sources/week11/numeric/numeric.cpp
@@ -15,6 +15,33 @@ int main()
     << std::accumulate(arr.begin(), arr.end(), 1, [](int a, int b) {return a + b; }) << std::endl;
   std::cout << "std::accumulate(arr.begin(), arr.end(), 1, [](int a, int b){return a * b;}) : "
     << std::accumulate(arr.begin(), arr.end(), 1, [](int a, int b) {return a * b; }) << std::endl;
+
+  std::vector<int> vec{ 1,2,3,4,5,6,7,8,9 };
+  std::vector<int> myVec;
+  std::cout << "adjacent difference : " << std::endl;
+  std::adjacent_difference(vec.begin(), vec.end(), std::back_inserter(myVec),
+    [](int a, int b) {return a * b; });
+  std::cout << "Contents of vec : ";
+  for (auto v : vec) std::cout << v << " ";
+  std::cout << std::endl;
+  std::cout << "Contents of myVec : ";
+  for (auto v : myVec) std::cout << v << " ";
+  std::cout << std::endl;
+
+  std::cout << "std::inner_product(vec.begin(), vec.end(), arr.begin(), 0) : " 
+    << std::inner_product(vec.begin(), vec.end(), arr.begin(), 0) << std::endl;
+
+  myVec = {};
+  std::partial_sum(vec.begin(), vec.end(), std::back_inserter(myVec));
+  std::cout << "Partial sum of vec : ";
+  for (auto v : myVec) std::cout << v << " ";
+  std::cout << std::endl;
+
+  std::cout << "iota : ";
+  std::vector<int> myLongVec(100);
+  std::iota(myLongVec.begin(), myLongVec.end(), 2000);
+  for (auto v : myLongVec) std::cout << v << " ";
+  std::cout << std::endl;
   return 0;
 }
 
